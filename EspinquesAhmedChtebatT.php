db7e620628cc780a31f1940c0d76f3b74271f4ef
ahmed <ahmed@ahmed>
2013-01-14 01:30:02 +0100

    ahmed chtebat ha subido el primer clase php

---
+++ EspinquesAhmedChtebatT.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of EspinquesAhmedChtebatT
+ *
+ * @author ahmed
+ */
+class EspinquesAhmedChtebatT extends Verdura {
+
+   var $concinada = false;
+
+   function Espinaca() 
+   {
+       $this->Verdura(true, "verde");
+   }
+
+   function cocinarla() 
+   {
+       $this->concinada = true;
+   }
+
+   function esta_cocinada() 
+   {
+       return $this->concinada;
+   }
+   
+ }
+
+?>
+
